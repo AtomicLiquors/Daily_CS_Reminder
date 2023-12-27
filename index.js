ac81e5aeb91047a4d2de7d3082081ead9485ae0a
chb <gyqls234@gmail.com>
2023-12-27 22:48:03 +0900

    :tada: bot init

---
+++ index.js
@@ -0,0 +1,30 @@
+// Watch my Discord Bot Project Tutorial video here: https://youtu.be/pDQAn18-2go - Discord Bot Tutorial | JavaScript & Node.js
+
+require('dotenv').config();
+
+const axios = require('axios');
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+
+client.on('ready', () => {
+    console.log('bot is ready');
+})
+
+client.on('messageCreate', async (message) => {
+    if (message.content === 'ping') {
+        message.reply({
+            content: 'pong',
+        })
+    }
+    else if (message.content === 'quote') {
+        let resp = await axios.get(`https://api.quotable.io/random`);
+        const quote = resp.data.content;
+
+        message.reply({
+            content: quote,
+        })
+    }
+})
+
+client.login(process.env.DISCORD_BOT_ID);
