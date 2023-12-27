a0283a2d9daacb7a7346a24d6cb41c338c6088ae
chb <gyqls234@gmail.com>
2023-12-28 02:02:39 +0900

    :tada: local test bot complete

--- src/index.js
+++ src/index.js
@@ -1,33 +1,57 @@
 // Watch my Discord Bot Project Tutorial video here: https://youtu.be/pDQAn18-2go - Discord Bot Tutorial | JavaScript & Node.js
 
-require('dotenv').config();
+require("dotenv").config();
+const cron = require("node-cron");
 
-const axios = require('axios');
-const { Client, GatewayIntentBits, EmbedBuilder, PermissionsBitField, Permissions } = require('discord.js');
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+const {
+  Client,
+  GatewayIntentBits,
+  Events,
+  EmbedBuilder,
+  PermissionsBitField,
+  Permissions,
+} = require("discord.js");
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
 
 
-client.on('ready', () => {
-    console.log('bot is ready');
-    client.user.setActivity('봇이 동작하고 있습니다!');
-})
+const timeOptions = { timeZone: "Asia/Seoul", hour12: false };
+const hourFormatter = new Intl.DateTimeFormat("en-US", { ...timeOptions, hour: "numeric" });
+const minuteFormatter = new Intl.DateTimeFormat("ko-KR", { ...timeOptions, minute: "numeric" });
 
-// client.login(TOKEN);
 
-client.on('messageCreate', async (message) => {
-    if (message.content === 'ping') {
-        message.reply({
-            content: 'pong',
-        })
-    }
-    else if (message.content === 'quote') {
-        let resp = await axios.get(`https://api.quotable.io/random`);
-        const quote = resp.data.content;
+client.once(Events.ClientReady, (x) => {
+  console.log(`${x.user.tag} is ready`);
+  client.user.setActivity("동작");
 
-        message.reply({
-            content: quote,
-        })
-    }
-})
+  const channel = client.channels.cache.get(process.env.CHANNEL_ID);
+  channel.send("봇 ON!");
+
+  cron.schedule("* * * * *", function () {
+    const currentDate = new Date();
+    const hours = Number(hourFormatter.format(currentDate));
+    const minutes = Number(minuteFormatter.format(currentDate));
+
+    console.log(hours);
+    console.log(minutes);
+
+    if(hours === 8 && minutes === 30)
+      channel.send(`현재 시간은 ${hours}시 ${minutes}분입니다.\n입실 체크하세요!`);
+    else if(hours === 18 && minutes === 0)
+      channel.send(`현재 시간은 ${hours}시 ${minutes}분입니다.\n퇴실 체크하세요!`);
+  });
+});
+
+/* 메시지에 답장하는 로직 */
+client.on(Events.MessageCreate, (msg) => {
+  if (msg.author.bot) return;
+  msg.channel.send('메시지에 응답합니다.');
+});
 
 client.login(process.env.DISCORD_BOT_ID);
+
