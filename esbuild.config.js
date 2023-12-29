b55a1c684714cf27dfab1e9e46fcf3f069c87d5b
chb <gyqls234@gmail.com>
2023-12-29 18:21:35 +0900

    :hammer:

--- esbuild.config.js
+++ esbuild.config.js
@@ -3,6 +3,6 @@ const {build} = require('esbuild');
 build({
   entryPoints: ['./src/index.js'],
   bundle: true,
-  outfile: './dist/bot.js',
+  outfile: './dist/index.js',
   platform: "node",
 });
